56194ee94aa5cf78681a9d5544c399c54064a04f
Birlax <birlax@gmail.com>
2018-04-03 02:09:22 +0530

    Simple School Algebra(Solving quadratic eq), Min/Max Area , Simple Geometry. Reviewer:Self

---
+++ src/codechef/J7/Main.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+// Do not change the class name or package
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int T = gi();
+		for (int i = 0; i < T; i++) {
+			int P = gi();
+			int S = gi();
+
+			double x1 = ((P * 1.0) / 12.0 - Math.sqrt((1.0 * P / 12.0) * (1.0 * P / 12.0) - (1.0 * S) / 6.0));
+			double x2 = ((P * 1.0) / 12.0 + Math.sqrt((1.0 * P / 12.0) * (1.0 * P / 12.0) - (1.0 * S) / 6.0));
+			double x = x1;
+
+			if (x1 < 0) {
+				x = x2;
+			} else {
+				x = x1;
+			}
+			;
+			double y = ((1.0 * P / 4.0) - (2.0 * x));
+			// System.out.println(x1 + " " + x2 + " " +x + " " + y);
+			double sol = x * x * y;
+
+			sol = (int) Math.round((sol * 100.00) + 0.005);
+			System.out.println(sol / 100.0);
+		}
+	}
+
+	public static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 32768);
+	public static StringTokenizer stringTokenizer = null;
+
+	public static int gi() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static long gl() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	public static double gd() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	public static String gs() throws IOException {
+		return next();
+	}
+
+	public static char gc() throws IOException {
+		return next().charAt(0);
+	}
+
+	public static String next() {
+		while (stringTokenizer == null || !stringTokenizer.hasMoreTokens()) {
+			try {
+				stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return stringTokenizer.nextToken();
+	}
+
+	public static StringBuilder ap(String str) {
+		answerStringBuilder.append(str);
+		return answerStringBuilder;
+	}
+
+	public static StringBuilder answerStringBuilder = new StringBuilder();
+
+}
