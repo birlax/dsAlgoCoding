4f956e8f480a2660ba2023d5204fbedd106878b6
Birlax <birlax@gmail.com>
2018-04-27 16:47:30 +0530

    Simple problem unable to find +/-1 error. Reviewer:Self

---
+++ src/codechef/unsolved/DECSTR/Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Do not change the class name or package
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int T = gi();
+		for (int t = 0; t < T; t++) {
+			int N = gi();
+			
+			N += N/26;
+			StringBuilder sb = new StringBuilder();
+			int c = 0;
+			for (int i = 0; i <= N; i++) {
+				sb.append((char) (c+'a'));
+				c++;
+				c = (c) % 26;
+			}
+			System.out.println(sb.reverse().toString());
+		}
+	}
+
+	public static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 32768);
+	public static StringTokenizer stringTokenizer = null;
+
+	public static int gi() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static long gl() throws IOException {
+		return Long.parseLong(next());
+	}
+
+	public static double gd() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	public static String gs() throws IOException {
+		return next();
+	}
+
+	public static char gc() throws IOException {
+		return next().charAt(0);
+	}
+
+	public static String next() {
+		while (stringTokenizer == null || !stringTokenizer.hasMoreTokens()) {
+			try {
+				stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return stringTokenizer.nextToken();
+	}
+
+	public static StringBuilder ap(String str) {
+		answerStringBuilder.append(str);
+		return answerStringBuilder;
+	}
+
+	public static StringBuilder answerStringBuilder = new StringBuilder();
+
+}
